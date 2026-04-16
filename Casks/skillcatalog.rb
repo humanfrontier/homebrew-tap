7140e8aa9da31a229a1f53f8e14434acd7d5c2b6
Salvatore Formisano <formisano.salvatore@gmail.com>
2026-04-16 23:35:21 +0200

    skillcatalog 0.1.0

--- Casks/skillcatalog.rb
+++ Casks/skillcatalog.rb
@@ -1,8 +1,8 @@
 cask "skillcatalog" do
   version "0.1.0"
-  sha256 "PLACEHOLDER"
+  sha256 "ea88bebe984d4c066c77f35af9e7e1705cf969a59877e8858b6069b7cd4983b7"
 
-  url "https://github.com/humanfrontier/skillcatalog-releases/releases/download/v#{version}/SkillCatalog_#{version}_universal.dmg"
+  url "https://github.com/humanfrontier/skillcatalog-releases/releases/download/v#{version}/SkillCatalog.app.tar.gz"
   name "SkillCatalog"
   desc "Git-native, local-first AI skill management"
   homepage "https://skillcatalog.dev"
@@ -10,7 +10,7 @@ cask "skillcatalog" do
   app "SkillCatalog.app"
 
   zap trash: [
-    "~/Library/Application Support/dev.skillcatalog.app",
-    "~/Library/Caches/dev.skillcatalog.app",
+    "~/Library/Application Support/com.skillcatalog.desktop",
+    "~/Library/Caches/com.skillcatalog.desktop",
   ]
 end
