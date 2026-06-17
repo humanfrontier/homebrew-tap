a641047a73380cb50d67a5545b4966786c90538e
sformisano <formisano.salvatore@gmail.com>
2026-06-17 18:57:36 +0200

    skillcatalog 0.7.1: notarized rebuild sha256

--- Casks/skillcatalog.rb
+++ Casks/skillcatalog.rb
@@ -1,6 +1,6 @@
 cask "skillcatalog" do
   version "0.7.1"
-  sha256 "38930c05c88b3139072f7765fba2028f0b63aa25b68c03bccf50a08dd7669680"
+  sha256 "9e14817c4d1bdcc21563f2e4d306baf41c8839a302a58c74c894cb22b904f0ee"
 
   url "https://github.com/humanfrontier/skillcatalog-releases/releases/download/v#{version}/SkillCatalog.app.tar.gz",
       verified: "github.com/humanfrontier/skillcatalog-releases/"
