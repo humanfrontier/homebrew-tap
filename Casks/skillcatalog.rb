0aa2b14b3fd2a3d18963392fbadd15c2efd44a18
sformisano <formisano.salvatore@gmail.com>
2026-06-18 23:19:37 +0200

    skillcatalog 0.7.5

--- Casks/skillcatalog.rb
+++ Casks/skillcatalog.rb
@@ -1,6 +1,6 @@
 cask "skillcatalog" do
-  version "0.7.4"
-  sha256 "3a1265b36e61b46b63cd9527022638cd4dc3a5e459615b0df761365350f8fbcd"
+  version "0.7.5"
+  sha256 "2c52c2fae50386bc1f90b9932c1be21a00cdd2e024955a02f011c0a36c731890"
 
   url "https://github.com/humanfrontier/skillcatalog-releases/releases/download/v#{version}/SkillCatalog.app.tar.gz",
       verified: "github.com/humanfrontier/skillcatalog-releases/"
